873f1d076c3eeb6acf4d03630064dfcba3872938
Dan Connor <danconn@danconnor.com>
2013-10-12 23:05:29 -0400

    bump video-js-resolutions version

--- video-js-resolutions/video-js-resolutions.js
+++ video-js-resolutions/video-js-resolutions.js
@@ -282,9 +282,11 @@ videojs.plugin('resolutions', function(options) {
       return this; // basically a no-op
     }
 
-    // remember our position in the current stream
-    var curTime = this.currentTime();
+    // remember our position and playback state
+    var curTime      = this.currentTime();
+    var remainPaused = this.paused();
 
+    // pause playback
     this.pause();
 
     // attempts to stop the download of the existing video
@@ -306,8 +308,11 @@ videojs.plugin('resolutions', function(options) {
       }));
 
       this.trigger('resolutionchange');
-      this.load();
-      this.play();
+
+      if (!remainPaused) {
+        this.load();
+        this.play();
+      }
 
       // remember this selection
       vjs.setLocalStorage('videojs_preferred_res', parseInt(new_source.index, 10));
